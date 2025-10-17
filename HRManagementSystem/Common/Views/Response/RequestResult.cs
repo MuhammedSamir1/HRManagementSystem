7885aea4e178765bb90b19cdc2fb062ad86ee2c2
Muhammed <Muhammed@MUHAMMED>
2025-10-17 19:00:58 +0300

    Modify

---
+++ HRManagementSystem/Common/Views/Response/RequestResult.cs
@@ -0,0 +1,14 @@
+﻿using HRManagementSystem.Common.Data.Enums;
+
+namespace HRManagementSystem.Common.Views.Response
+{
+    public record RequestResult<T>(T data, bool isSuccess, string message, ErrorCode errorCode)
+    {
+        public static RequestResult<T> Success(T data, string message = "Request was successful") =>
+            new RequestResult<T>(data, true, message, ErrorCode.None);
+        public static RequestResult<T> Failure(string message, ErrorCode errorCode = ErrorCode.None) =>
+            new RequestResult<T>(default!, false, message, errorCode);
+        public static RequestResult<T> Failure(ErrorCode errorCode) =>
+            new RequestResult<T>(default!, false, errorCode.ToString(), errorCode);
+    }
+}
--- HRManagementSystem/Features/OrganizationManagement/AddOrginzation/Commands/AddOrganizationCommand.cs
+++ HRManagementSystem/Features/OrganizationManagement/AddOrginzation/Commands/AddOrganizationCommand.cs
@@ -8,22 +8,22 @@ using MediatR;
 namespace HRManagementSystem.Features.OrganizationManagement.AddOrganization.Commands
 {
     public record AddOrganizationCommand(string Name, string? LegalName, string? Industry,
-        string? DefaultTimezone, string? DefaultCurrency, AddAddressDto AddressDto) : IRequest<ResponseDto<bool>>;
+        string? DefaultTimezone, string? DefaultCurrency, AddAddressDto AddressDto) : IRequest<RequestResult<bool>>;
 
-    public class AddOrganizationCommandHandler : RequestHandlerBase<AddOrganizationCommand, ResponseDto<bool>, Organization, int>
+    public class AddOrganizationCommandHandler : RequestHandlerBase<AddOrganizationCommand, RequestResult<bool>, Organization, int>
     {
         public AddOrganizationCommandHandler(RequestHandlerBaseParameters<Organization, int> parameters) : base(parameters)
         {
         }
 
-        public override async Task<ResponseDto<bool>> Handle(AddOrganizationCommand request, CancellationToken ct)
+        public override async Task<RequestResult<bool>> Handle(AddOrganizationCommand request, CancellationToken ct)
         {
             var organization = _mapper.Map<AddOrganizationCommand, Organization>(request);
 
             var isAdded = await _generalRepo.AddAsync(organization, ct);
 
-            if (!isAdded) return ResponseDto<bool>.Failure("Organization wasn't added successfully!", ErrorCode.InternalServerError);
-            return ResponseDto<bool>.Success(isAdded, "Organization added successfully!");
+            if (!isAdded) return RequestResult<bool>.Failure("Organization wasn't added successfully!", ErrorCode.InternalServerError);
+            return RequestResult<bool>.Success(isAdded, "Organization added successfully!");
         }
     }
 }
